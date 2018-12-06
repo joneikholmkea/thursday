d1a09814af24d9b14b0f7a66db141f252263e76e
joneikholm <joneikholm@gmail.com>
2018-12-06 14:59:11 +0100

    first

---
+++ src/main/java/com/example/thursday/HomeController.java
@@ -0,0 +1,17 @@
+package com.example.thursday;
+
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class HomeController {
+
+    @ResponseBody
+    @GetMapping("/")
+    public String index(){
+        return "<h2>You've made it!</h2>";
+    }
+
+}
